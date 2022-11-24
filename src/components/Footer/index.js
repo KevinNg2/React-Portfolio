d3e4831813a96532f7a70b21ae18b8510144e0ab
Kevin Ng <102876076+KevinNg2@users.noreply.github.com>
2022-11-23 21:22:27 -0800

    images, components, functionality

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div class="footer" id="footer">
+            <a href="https://github.com/KevinNg2"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" class="icon"/></a>
+            <a href="https://www.linkedin.com/in/kevinng3292/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
+            <a href="https://www.instagram.com/kevin_ng___/"><img src="https://img.icons8.com/color/48/000000/instagram.png" alt="Instagram" class="icon"/></a>
+            <p>
+                &copy; Kevin Ng  | <a href="file:///C:/Users/19089/Downloads/Kevin%20Ng%20SE%20Resume%20.pdf" class="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">color palette</a>  |  designed with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>  
+            </p>
+
+
+        </div>
+    )
+}
+
+export default Footer;

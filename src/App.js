084c4e5dcc524d2baa4f44607ee36299ed148f5b
Kevin Ng <102876076+KevinNg2@users.noreply.github.com>
2022-12-29 20:09:40 -0800

    images, components, functionality

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+// Run App
+function App () {
+    return (
+        <div className="App">
+            <Header/>
+            
+        </div>
+    );
+}
+
+export default App;
